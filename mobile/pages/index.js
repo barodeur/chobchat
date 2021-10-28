02511afb79b8e1c7f2bbfaf54d4d6587f0b659e9
Paul Chobert <paul@chobert.fr>
2021-10-28 14:36:59 -0400

    Move pages folder

--- mobile/pages/index.js
+++
@@ -1,6 +0,0 @@
-// @generated: @expo/next-adapter@3.1.6
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { make as App } from "../src/App.bs";
-
-export default App;
--- mobile/src/pages/_document.js
+++ mobile/src/pages/_document.js
@@ -1,2 +1,1 @@
-// @generated: @expo/next-adapter@3.1.6
 export { default } from '@expo/next-adapter/document';
