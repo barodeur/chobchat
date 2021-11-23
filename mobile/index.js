b77f2ed9fce6d1414f2963927d1f5ae6cf28eb0c
Paul Chobert <paul@chobert.fr>
2021-11-22 19:05:10 -0500

    Remove App.js wrapper

--- mobile/index.js
+++ mobile/index.js
@@ -1,11 +1,5 @@
-import 'expo-dev-client';
+require('react-native-gesture-handler');
+const { registerRootComponent } = require('expo');
+const { make: App } = require('./src/App.bs');
 
-import 'react-native-gesture-handler';
-import { registerRootComponent } from 'expo';
-
-import App from './App';
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
 registerRootComponent(App);
