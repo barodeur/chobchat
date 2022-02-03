38bbe8907cd5b9706ff547aea01651a6ad748d90
Paul Chobert <paul@chobert.fr>
2022-02-03 15:59:20 -0500

    Use separate babel presets for metro and web

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,13 +1,26 @@
 // @generated: @expo/next-adapter@3.1.6
 // Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#shared-steps
 
-module.exports = {
-  plugins: [
-    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
-    // The following line should be uncomented to silence some nextjs warnings,
-    // but when uncommented, it causes FlatList to raise an error,
-    // see https://stackoverflow.com/questions/69922302
-    // ["@babel/plugin-proposal-private-methods", { "loose": true }]
-  ],
-  presets: ["@expo/next-adapter/babel", "jotai/babel/preset"]
-};
+module.exports = (api) => {
+  api.cache.using(() => process.env.NODE_ENV)
+
+  const isMetro = api.caller(caller => caller && caller.name === "metro");
+
+  const nextPreset = "next/babel";
+  const expoPreset = [
+    require("babel-preset-expo"),
+    {
+      web: { useTransformReactJsxExperimental: true },
+      // Disable the `no-anonymous-default-export` plugin in babel-preset-expo
+      // so users don't see duplicate warnings.
+      "no-anonymous-default-export": false,
+    },
+  ];
+
+  return {
+    presets: [
+      isMetro ? expoPreset : nextPreset,
+      "jotai/babel/preset"
+    ]
+  }
+}
